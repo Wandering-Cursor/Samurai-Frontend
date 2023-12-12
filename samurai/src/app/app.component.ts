2117a8ede278e760c80836c8a7f8dde3ecb6bd61
Artem Smotrych <artem.smotrich@c-pro.nl>
2023-12-12 19:40:37 +0100

    feat: init call #17

--- samurai/src/app/app.component.ts
+++ samurai/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { ApiService } from './services/api/api.service';
+import { Init } from './interfaces/init';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,5 +11,26 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(
+    private svc: ApiService
+  ) {
+  }
   title = 'samurai';
+
+  ngOnInit() {
+    this.svc.initGet().subscribe({
+      next: (data: Init) => {
+        console.log('Data received:', data);
+        // Do something with the data
+      },
+      error: (error) => {
+        console.error('Error:', error);
+        // Handle error
+      },
+      complete: () => {
+        console.log('Request completed');
+        // Handle completion
+      }
+    });
+  }
 }
--- samurai/src/main.ts
+++ samurai/src/main.ts
@@ -1,6 +1,12 @@
+// main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { ApiService } from './app/services/api/api.service';
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    ApiService,
+  ]
+}).catch(err => console.error(err));
